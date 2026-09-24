f789636ee50551fbc29fb7ff827cdb8f0fdda668
Steve Freund <sdfreund10@gmail.com>
2026-09-23 20:51:01 -0700

    Add js function to add the timezone to a cookie and add middleware to read that cookie and set the timezone

--- src/gym_journal/tests/test_middleware.py
+++ src/gym_journal/tests/test_middleware.py
@@ -1,7 +1,15 @@
-from django.test import Client, TestCase
-from django.urls import reverse
+from zoneinfo import ZoneInfo
 
-from gym_journal.middleware import SKIP_PATH_PREFIXES
+from django.http import HttpResponse
+from django.test import Client, RequestFactory, SimpleTestCase, TestCase
+from django.urls import reverse
+from django.utils import timezone
+
+from gym_journal.middleware import (
+    SKIP_PATH_PREFIXES,
+    TIMEZONE_COOKIE_NAME,
+    TimeZoneMiddleware,
+)
 
 
 class RequestLoggingMiddlewareTests(TestCase):
@@ -32,3 +40,57 @@ class RequestLoggingMiddlewareTests(TestCase):
     def test_skip_path_prefixes(self):
         self.assertIn("/health/", SKIP_PATH_PREFIXES)
         self.assertIn("/static/", SKIP_PATH_PREFIXES)
+
+
+class TimeZoneMiddlewareTests(SimpleTestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def tearDown(self):
+        timezone.deactivate()
+
+    def _run_with_cookie(self, cookie_value):
+        activated = []
+
+        def get_response(_request):
+            activated.append(timezone.get_current_timezone_name())
+            return HttpResponse("ok")
+
+        middleware = TimeZoneMiddleware(get_response=get_response)
+        request = self.factory.get("/")
+        if cookie_value is not None:
+            request.COOKIES[TIMEZONE_COOKIE_NAME] = cookie_value
+        middleware(request)
+        return activated[0] if activated else None
+
+    def test_url_encoded_cookie_activates_timezone(self):
+        active_tz = self._run_with_cookie("America%2FLos_Angeles")
+
+        self.assertEqual(active_tz, "America/Los_Angeles")
+        self.assertEqual(timezone.get_current_timezone_name(), "UTC")
+
+    def test_plain_cookie_activates_timezone(self):
+        active_tz = self._run_with_cookie("America/Los_Angeles")
+
+        self.assertEqual(active_tz, "America/Los_Angeles")
+        self.assertEqual(timezone.get_current_timezone_name(), "UTC")
+
+    def test_invalid_cookie_deactivates_timezone(self):
+        timezone.activate(ZoneInfo("Europe/London"))
+        active_tz = self._run_with_cookie("Not%2FA%2FZone")
+
+        self.assertEqual(active_tz, "UTC")
+        self.assertEqual(timezone.get_current_timezone_name(), "UTC")
+
+    def test_missing_cookie_deactivates_timezone(self):
+        timezone.activate(ZoneInfo("Europe/London"))
+        active_tz = self._run_with_cookie(None)
+
+        self.assertEqual(active_tz, "UTC")
+        self.assertEqual(timezone.get_current_timezone_name(), "UTC")
+
+    def test_timezone_is_deactivated_after_response(self):
+        active_tz = self._run_with_cookie("America%2FNew_York")
+
+        self.assertEqual(active_tz, "America/New_York")
+        self.assertEqual(timezone.get_current_timezone_name(), "UTC")
